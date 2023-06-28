8af82bcf59e18f3840c8c7aa385179e6fdb20a5d
DIR\yusuke.inuma <yusuke.inuma@accenture.com>
2023-06-28 10:50:19 +0900

    insert initialize data

--- hr_tool/admin.py
+++ hr_tool/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (MCareerLevel, MDte, MHomeoffice, MIndustry, MSkill,
+                     MSkillCategory, TAccount, TAssignExp, TPreCareer,
+                     TProject, TSkill, TTraining, TTrainingExp, User)
+
+admin.site.register(MCareerLevel)
+admin.site.register(MDte)
+admin.site.register(MHomeoffice)
+admin.site.register(MIndustry)
+admin.site.register(MSkill)
+admin.site.register(MSkillCategory)
+admin.site.register(TAccount)
+admin.site.register(TAssignExp)
+admin.site.register(TPreCareer)
+admin.site.register(TProject)
+admin.site.register(TSkill)
+admin.site.register(TTraining)
+admin.site.register(TTrainingExp)
+admin.site.register(User)
