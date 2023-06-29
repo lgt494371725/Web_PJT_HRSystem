aa19e2c3872c0e1195c007c5581b7fbd147ee3cd
yiying.jin <yiying.jin@accenture.com>
2023-06-29 13:37:43 +0900

    yiying

--- hr_tool/views.py
+++ hr_tool/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
+from .models import User
+from .forms import SearchForm
 
 # Create your views here.
 def employee_list(request):
-    return render(request, 'list.html')
+    employees = User.objects.all()
+    context = {
+        'employees':employees
+    }
+    
+    return render(request, 'list.html',context)
 
 
 def detail(request, pk):
     return render(request, 'detail.html')
+
+def index(request):
+    form = SearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['id']
+        employees = User.objects.filter(id=query)
+    else:
+        print("!!!error:",form.errors)
+    context = {
+        'employees': employees,
+        'form': form
+
+    }
+    return render(request, 'hr_tool/list.html',context)
