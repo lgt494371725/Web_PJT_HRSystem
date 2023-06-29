32c3db39094103534ae231ca8d0d2de1aa44414a
yiying.jin <yiying.jin@accenture.com>
2023-06-29 16:47:29 +0900

    yiying

--- hr_tool/forms.py
+++ hr_tool/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class SearchForm(forms.Form):
-    search  = forms.CharField(required=False)
+    Search  = forms.CharField(required=False)
     def __init__(self, *args, **kwargs):
         super(SearchForm, self).__init__(*args, **kwargs)
--- hr_tool/views.py
+++ hr_tool/views.py
@@ -1,30 +1,42 @@
 from django.shortcuts import render
 from .models import User
 from .forms import SearchForm
+from django.db import models
 
 # Create your views here.
 def employee_list(request):
-    employees = User.objects.all()
+    form = SearchForm(request.GET)
+    employees = User.objects.all() 
+    print(form)
+    if form.is_valid():
+        query = form.cleaned_data['Search']
+        print('abc',query)
+        employees = User.objects.all() 
+        employees = employees.filter(models.Q(id__icontains=query))
+    else:
+        print("!!!error:",form.errors)
+        employees = User.objects.all() 
     context = {
-        'employees':employees
+        'employees':employees,
+        'form': form
     }
-    
     return render(request, 'list.html',context)
 
 
 def detail(request, pk):
     return render(request, 'detail.html')
 
-def index(request):
-    form = SearchForm(request.GET)
-    if form.is_valid():
-        query = form.cleaned_data['id']
-        employees = User.objects.filter(id=query)
-    else:
-        print("!!!error:",form.errors)
-    context = {
-        'employees': employees,
-        'form': form
-
-    }
-    return render(request, 'hr_tool/list.html',context)
+# def index(request):
+#     form = SearchForm(request.GET)
+#     print(form)
+#     if form.is_valid():
+#         query = form.cleaned_data['search']
+#         employees = User.objects.filter(id=query)
+#         employees = User.objects.all()
+#     else:
+#         print("!!!error:",form.errors)
+#     context = {
+#         'employees': employees,
+#         'form': form
+#     }
+#     return render(request, 'hr_tool/list.html',context)
