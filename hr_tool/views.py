ff43145907c7426ead1eec1312c9407d627c4d3a
DIR\hirofumi.a.murakami <hirofumi.a.murakami@accenture.com>
2023-06-29 14:42:51 +0900

    signup画面追加

--- hr_tool/views.py
+++ hr_tool/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 # Create your views here.
 def employee_list(request):
@@ -7,3 +11,23 @@ def employee_list(request):
 
 def detail(request, pk):
     return render(request, 'detail.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'hr_user/signupuser.html', {'form': UserCreationForm()})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    request.POST['username'],
+                    password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('detial')
+            except IntegrityError:
+                return render(request, 'hr_user/signupuser.html', 
+                              {'form': UserCreationForm(), 'error': 'Password didnt match.'}
+                              )
+
+def signup(request):
+    return render(request, 'hr_tool/signupuser.html')
